a2926ad5f84faa81a5eaaeea5a884e2863bc9e03
JangHJ <yg9316@swu.ac.kr>
2023-07-06 00:08:00 +0900

    레벨1 문제풀이 수정완료

--- Level1/이상한 문자 만들기.java
+++ Level1/이상한 문자 만들기.java
@@ -1,19 +1,23 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] arr = s.split(" ");
+        String[] arr = s.split("");
         
+        int idx = 0;
         for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[i].length(); j++)
-            {
-                if((j+1) % 2 == 0)
-                    answer += arr[i].charAt(j);
-                else
-                    answer += Character.toUpperCase(arr[i].charAt(j));
+            if(arr[i].equals(" "))
+                idx = 0;
+            else if(idx % 2 == 0){ // 홀수일 때 0, 2, 4
+                arr[i] = arr[i].toUpperCase();
+                idx++;
+            }  
+            else{
+                arr[i] = arr[i].toLowerCase();
+                idx++;
             }
-            if(i < arr.length-1)
-                answer += " ";
+            answer += arr[i];
         }
+        
         return answer;
     }
 }
