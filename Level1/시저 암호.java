56372d2fea531094e5b81295d1e15a9b36cfbbab
JangHJ <yg9316@swu.ac.kr>
2023-07-11 23:11:08 +0900

    레벨1 문제풀이 (수정완료)

--- Level1/시저 암호.java
+++ Level1/시저 암호.java
@@ -2,11 +2,23 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         char[] ch = s.toCharArray();
-        
+
         for(char c : ch) {
             if(c != ' ') {
-                if(c == 'Z' || c == 'z') c -= 26;
-                c += n;
+                int c2 = (int)c + n;
+                if(Character.isUpperCase(c)){ // 대문자 A-Z 65~90
+                    if(c2 > 90){
+                        c = '@';
+                        c += (c2 - 90);   
+                    }
+                    else c += n;
+                }else if(Character.isLowerCase(c)){ // 소문자 a-z 97~122
+                    if(c2 > 122){
+                        c = '`';
+                        c += (c2 - 122);   
+                    }
+                    else c += n;
+                }
                 answer += c;
             } else
                 answer += " ";
